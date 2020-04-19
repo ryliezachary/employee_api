e80b33e0fa8c5760e225d8037170000f9ad1f040
ryliezachary <losganoryan0808@gmail.com>
2020-04-20 01:19:14 +0800

    a

--- config.php
+++ config.php
@@ -1,13 +1,14 @@
 <?php
+
 class Connect extends PDO
 {
 	public function __construct()
 	{
-	parent:: __construct("mysql:host=us-cdbr-iron-east-01.cleardb.net; dbname=heroku_8ab616a97278496", 'b395d6de31dcf5', '9fa27eb9',
-	array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-	$this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$this->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-}
+		parent::__construct("mysql:host=us-cdbr-iron-east-01.cleardb.net; dbname=heroku_8ab616a97278496", 'b395d6de31dcf5', '9fa27eb9',
+		array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+		$this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$this->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+	}
 }
 
 ?>
@@ -19,3 +20,5 @@ USERNAME : b395d6de31dcf5
 Password : 9fa27eb9
 host : us-cdbr-iron-east-01.cleardb.net
 db name ; heroku_8ab616a97278496 -->
+
+
